60fa5ce25ff14f69cd3b379b9128f02da61fb2bd
seantyh <seantyh@crl.psy.ntu.edu.tw>
2017-08-03 14:31:13 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='KorFeatures',
+      version='0.1',
+      description='Analyze features for Kor projects',
+      url='http://github.com/seantyh/KorFeatures',
+      author='Sean Tseng',
+      author_email='seantyh@gmail.com',
+      license='MIT',
+      packages=['KorFeatures'],
+      zip_safe=False)
+
